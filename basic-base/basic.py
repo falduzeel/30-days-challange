563ee1b9759c0166bb64672df03f3bf6f5326313
Zeel faldu <falduzeel54@gmail.com>
2026-09-02 19:45:41 +0530

    basic-base directory

--- basic-base/basic.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name? ").strip().title()
-
-if name:
-    print(f"Hello, {name}! Great to meet you.")
-    print(f"Informative tidbit: Your name is {len(name)} letters long.")
-else:
-    print("Hello, mystery guest! You didn't type a name.")
